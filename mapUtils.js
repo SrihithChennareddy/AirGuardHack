583c285c38a08ce22d24a090b81b7f679bbb562e
Srihith Chennareddy <srihith.chennareddy@gmail.com>
2025-02-17 18:57:11 -0800

    Delete mapUtils.js

--- mapUtils.js
+++
@@ -1,7 +0,0 @@
-// Convert miles to meters for Google Maps
-export const milesToMeters = (miles) => miles * 1609.34;
-
-// Convert radius to appropriate zoom level
-export const getZoomLevel = (radius) => {
-  return Math.round(14 - Math.log(radius) / Math.log(2));
-}; 
